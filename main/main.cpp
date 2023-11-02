cbda5add270498769f9bcca808978d30c747e37e
PascalHnkl <116684723+PascalHnkl@users.noreply.github.com>
2023-11-02 19:05:22 +0100

    Add files via upload

--- main/main.cpp
+++ main/main.cpp
@@ -14,16 +14,17 @@ std::ostream &PutTime() {
 
 int main(int argc, char **argv) {
   TApplication app("app", &argc, argv); // added by Michael to resolve seg fault in root 5
-  TString AMBER_DAQ_RECO_FILE = "test_data/phast.u7001.301954.root";
-  TString VMM_DAQ_RECO_FILE = "test_data/run201_20230926235051_woHits.root"; //"AMBER_2023_data/run314025_20230926155547_woHits.root";
+  TString AMBER_DAQ_RECO_FILE = "/mnt/data/compass/phast.u7001.302117.root";
+  TString VMM_DAQ_RECO_FILE = "/mnt/data/compass/run303925_20230921234249_woHits.root"; //"AMBER_2023_data/run314025_20230926155547_woHits.root";
 
   PutTime() << ": Get AMBER data..." << std::endl;
-  //AmberData AMBER(AMBER_DAQ_RECO_FILE.Data());
+  AmberData AMBER(AMBER_DAQ_RECO_FILE.Data());
   PutTime() << ": Get VMM data..." << std::endl;
   VMMData VMM(VMM_DAQ_RECO_FILE.Data());
 
   PutTime() << ": Amber time stamps..." << std::endl;
-  //AMBER.PlotTimeStamps(12);
+  AMBER.PlotTimeStamps(12);
+  double x = AMBER.CalibrateAmberTrigger(16, VMM.GetTriggerTimes(), 3189e09);
   //AMBER.PlotTimeStamps(13);
 // Takes a bit
 //   PutTime() << ": Amber cluster time diff..." << std::endl;
@@ -35,9 +36,10 @@ int main(int argc, char **argv) {
   VMM.PlotClusterTimes();
 // Takes quite long
    PutTime() << ": VMM cluster trigger time diff..." << std::endl;
-   VMM.PlotClusterTriggerDiff();
+   //VMM.PlotClusterTriggerDiff();
   PutTime() << ": VMM cluster trigger 2d..." << std::endl;
-  VMM.PlotClusterTrigger2D();
+  //VMM.PlotClusterTrigger2D();
+  PutTime() << "  " << x << std::endl;
   PutTime() << ": End of program!" << std::endl;
   app.Terminate(); // addem by Michael to properly clode TApplication in root 5 to prevent seg fault
   return 0;
