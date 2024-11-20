2c1d39e9d005212c0a7ad3c7e292154aed6544d3
PascalHnkl <116684723+PascalHnkl@users.noreply.github.com>
2024-11-20 13:56:53 +0100

    Add files via upload

--- include/ReadAmberData.h
+++ include/ReadAmberData.h
@@ -4,9 +4,24 @@
 #include <vector>
 
 #include "TROOT.h"
+#include "TrackData.h"
+
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+
+
 
 struct PerSpill {
   std::vector<double> timeStamps;
+  std::vector<float> TCS;
+  std::vector<double> meanTrackTime;;
+  std::vector<double> sigmaTrackTime;
+  std::vector<double> tracks_x;
+  std::vector<double> tracks_y;
+  std::vector<double> tracks_sigma_x;
+  std::vector<double> tracks_sigma_y;
+  std::vector<double> tracks_z;
+  std::vector<bool> boundaries_cut;
   Long64_t SpillNumber;
 };
 
@@ -17,11 +32,20 @@ public:
   /*Make a plot where all time stamps of amber trigger data for a given spill is shown*/
   void PlotTimeStamps(Long64_t spill) const;
 
+  void TrackTime(Long64_t spill) const;
+
   /*Plot the difference of all time stamps of amber in a given time frame to vmm time stamps*/
-  void PlotClusterTimeDiff(Long64_t spill, const std::vector<double> &VMMTimeStamps);
+  void PlotClusterTimeDiff(Long64_t spill, const std::vector<double> &VMMTimeStamps, double timeshift);
+
+
+  std::tuple<double, std::vector<bool>, std::vector<double>, std::vector<double>, std::vector<double>> CalibrateAmberTrigger(Long64_t VMM_scan_number, Long64_t AMBER_run_number, Long64_t spill, const std::vector<double>& TriggerTimeStamps, double spill_pos);
+
+  void Residuals(Long64_t VMM_scan_number, Long64_t AMBER_run_number, Long64_t spill, const std::vector<int>& VMMxfec, const std::vector<int>& VMMyfec, const std::vector<double>& VMMxTimeStamps, const std::vector<double>& VMMyTimeStamps, const std::vector<double>& VMM_xpos, const std::vector<double>& VMM_ypos, const std::vector<int>& x_adc, const std::vector<int>& y_adc, const std::vector<int>& x_size, const std::vector<int>& y_size, double timeshift, const std::vector<bool>& tracktime_matched, const std::vector<double>& tracktime, const std::vector<double>& trigger_TCS, const std::vector<double>& TimeInSpill_calibrated);
 
 private:
   std::vector<PerSpill> mPerSpillData;
   // clock correction
-  double alpha = 3.218e-5;
+  double alpha = 3.218e-5;// + 0.06e-5;
+  TTreeReaderValue<std::vector<TrackData>> *spectrometerTracks;
+  TTreeReader *treeReader = nullptr;
 };
--- include/ReadVmmData.h
+++ include/ReadVmmData.h
@@ -5,7 +5,11 @@
 
 class VMMData {
 public:
-  VMMData(const std::string &DataFile);
+  VMMData(const std::string &DataFile_cluster, const std::string &DataFile_trigger, int spill);
+
+  /*Plot all vmm cluster positions*/
+  void PlotCluster0Pos() const;
+  void PlotCluster1Pos() const;
 
   /*Plot all vmm cluster times*/
   void PlotClusterTimes() const;
@@ -15,13 +19,55 @@ public:
 
   /*Plot the difference of all amber triggers with all vmm cluster times*/
   void PlotClusterTriggerDiff() const;
+  void Synch_fecs(double shift_1, double shift_2, double shift_4, double shift_5);
+
 
   /*Make 4 plots to show the distribution of cluster times and amber triggers. Just in the spills*/
   void PlotClusterTrigger2D() const;
 
-  const std::vector<double> &GetClusterTimes() const { return mClusterTimes; }
+  const std::vector<double>& GetTriggerTimes() const { return mTriggerTimes; }
+
+  const std::vector<double> &GetCluster0Times() const { return mCluster0Times; }
+
+  const std::vector<double>& GetCluster1Times() const { return mCluster1Times; }
+
+  const std::vector<double> &GetCluster0Pos() const { return mCluster0Pos; }
+
+  const std::vector<double>& GetCluster1Pos() const { return mCluster1Pos; }
+
+  const std::vector<int>& GetCluster0fec() const { return mCluster0fec; }
+
+  const std::vector<int>& GetCluster1fec() const { return mCluster1fec; }
+
+  const std::vector<int>& GetCluster0adc() const { return mCluster0adc; }
+
+  const std::vector<int>& GetCluster1adc() const { return mCluster1adc; }
+
+  const std::vector<int>& GetCluster0size() const { return mCluster0size; }
+
+  const std::vector<int>& GetCluster1size() const { return mCluster1size; }
+
+  const std::vector<double>& GetHitTime() const { return mHitTime; }
+
+  const std::vector<int>& GetHitFEC() const { return mHitFEC; }
+
+  const std::vector<int>& GetHitVMM() const { return mHitVMM; }
+
+
 
 private:
-  std::vector<double> mClusterTimes;
+  std::vector<double> mCluster0Times;
+  std::vector<double> mCluster1Times;
+  std::vector<double> mCluster0Pos;
+  std::vector<double> mCluster1Pos;
+  std::vector<int> mCluster0fec;
+  std::vector<int> mCluster1fec;
+  std::vector<int> mCluster0adc;
+  std::vector<int> mCluster1adc;
+  std::vector<int> mCluster0size;
+  std::vector<int> mCluster1size;
   std::vector<double> mTriggerTimes;
+  std::vector<double> mHitTime;
+  std::vector<int> mHitFEC;
+  std::vector<int> mHitVMM;
 };
